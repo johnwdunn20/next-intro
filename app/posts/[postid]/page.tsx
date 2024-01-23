336d3744979da5bfea21c0a3033208c9ca8c99f9
John Dunn <johnwdunn20@gmail.com>
2024-01-23 14:30:57 -0800

    dynamic pages

---
+++ app/posts/[postid]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// to ceate a dynamic route, create a folder with square brackets and add a page file
+
+const page = () => {
+  return (
+    <div>{postId}</div>
+  )
+}
+
+export default page
---
+++ app/user/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// to create a new page, create a folder and add a page file
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
