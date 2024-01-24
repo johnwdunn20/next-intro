f38285e6dcceb1aad135edbfe8cdecc2423201c4
John Dunn <johnwdunn20@gmail.com>
2024-01-23 19:47:05 -0800

    updated auth

--- app/api/auth/[...nextAuth]/route.ts
+++ app/api/auth/[...nextAuth]/route.ts
@@ -1,2 +1,34 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import Auth0Provider from "next-auth/providers/auth0";
+
+// console.log('CLIENTID: ', process.env.GOOGLE_CLIENT_ID);
+
+// configured google here: https://console.cloud.google.com/apis/credentials/consent?project=social-media-412203
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    }),
+    // Auth0Provider({
+    //   clientId: process.env.AUTH0_CLIENT_ID || '',
+    //   clientSecret: process.env.AUTH0_CLIENT_SECRET || '',
+    //   issuer: process.env.AUTH0_DOMAIN || '',
+    // }),
+  ],
+
+  async session({session}) {
+
+  },
+  async signIn({ profile }) {
+    try {
+      
+    } catch (e) {
+
+    }
+  }
+});
+
+export { handler as GET, handler as POST };
---
+++ utils/database.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDatabase = async () => {
+  mongoose.set('strictQuery', true);
+
+  if (isConnected) {
+    console.log('=> using existing database connection');
+    return Promise.resolve();
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "social-media",
+      usenewUrlParser: true,
+      setUnifiedTopology: true,
+    })
+
+    isConnected = true;
+    console.log('=> using new database connection');
+
+  } catch (e) {
+    console.log('=> an error occurred when connecting to the database', e);
+  }
+}
